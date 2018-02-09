a2d1080b2a1e0d2c06d8426bc3e0609302a6b1cb
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2018-02-09 13:32:50 -0700

    worked on the frame more

--- src/flashcard/view/FlashcardFrame.java
+++ src/flashcard/view/FlashcardFrame.java
@@ -1,6 +1,30 @@
 package flashcard.view;
 
-public class FlashcardFrame
+import flashcard.controller.FlashcardController;
+import javax.swing.JFrame;
+
+public class FlashcardFrame extends JFrame
 {
 
+	private FlashcardController appController;
+	private FlashcardPanel appPanel;
+	
+	public FlashcardFrame(FlashcardController appController)
+	{
+		super();
+		this.appController = appController;
+//		this.appPanel = new FlashcardPanel(appController);
+		
+		setupFrame();
+	}
+	
+	public void setupFrame()
+	{
+//		this.setContentPane(appPanel);
+		this.setTitle("FlashCards");
+		this.setSize(100, 100);
+		this.setResizable(false);
+		
+		this.setVisible(true);
+	}
 }
